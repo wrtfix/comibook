6a4bd87b6f513cd08bd6109c8a8998689f224277
wrtfix <wrtfix@gmail.com>
2016-01-30 13:36:07 -0300

    Agrego archivos a ignorar

--- application/config/database.php
+++ application/config/database.php
@@ -49,7 +49,7 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'wrtfix';
+$db['default']['username'] = 'root';
 $db['default']['password'] = 'hanna64';
 $db['default']['database'] = 'comibook';
 $db['default']['dbdriver'] = 'mysql';
