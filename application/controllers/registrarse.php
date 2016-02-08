4f7dd93543aa876955f822705da9f887b38c8349
wrtfix <wrtfix@gmail.com>
2016-02-08 13:14:32 -0300

    Agrego captcha al formulario registrarse

--- application/controllers/registrarse.php
+++ application/controllers/registrarse.php
@@ -16,10 +16,26 @@ class Registrarse extends CI_Controller
 		$this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean|is_unique[users.username]');
     	$this->form_validation->set_rules('password', 'Password', 'required|matches[passconf]');
 		$this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
+	 	$this->form_validation->set_rules('userCaptcha', 'Captcha', 'required|callback_check_captcha');
+    	$userCaptcha = $this->input->post('userCaptcha');
+    	//Generacion de captch
+		$this->load->helper('captcha');
+  		$random_number = substr(number_format(time() * rand(),0,'',''),0,6);
+  		$vals = array(
+         'word' => $random_number,
+         'img_path' => './captcha/',
+         'img_url' => base_url().'captcha/',
+         'img_width' => 140,
+         'img_height' => 32,
+         'expiration' => 7200
+        );
+  		$data['captcha'] = create_captcha($vals);
+  		$this->session->set_userdata('captchaWord',$data['captcha']['word']);
+  		//Fin de generacion de captch
+
 		$data['userAdd'] = null; 
 		if($this->form_validation->run() == FALSE)
     	{
-      		//Field validation failed.  User redirected to login page
       		$data['page'] = 'registrarse';
       		$this->layout->view('pages/registrarse', $data);
       		
@@ -35,5 +51,16 @@ class Registrarse extends CI_Controller
     	}
 	
 	}
+
+	public function check_captcha($str){
+	    $word = $this->session->userdata('captchaWord');
+	    if(strcmp(strtoupper($str),strtoupper($word)) == 0){
+	      return true;
+	    }
+	    else{
+	      $this->form_validation->set_message('check_captcha', 'Please enter correct words!');
+	      return false;
+	    }
+  }
 
 }
--- application/views/pages/registrarse.php
+++ application/views/pages/registrarse.php
@@ -7,20 +7,21 @@
               <a class="close" data-dismiss="alert" href="#">×</a><?php echo validation_errors(); ?>
           </div>
     <?php } ?>
-
     <?php if ($userAdd !=null) { ?>
           <div class="alert alert-info alert-dismissable">
               <a class="close" data-dismiss="alert" href="#">×</a><?php echo $userAdd; ?>
           </div>
     <?php } ?>
-    
-
     <?php echo form_open('registrarse'); ?>
       <input type="text" id="username" class="span4" name="username" placeholder="Nombre de Usuario"/> </br>
       <input type="password" id="password" class="span4" name="password" placeholder="Clave"/> </br>
       <input type="password" id="passconf" class="span4" name="passconf" placeholder="Re Clave"/> </br>
       <input type="email" id="email" class="span4" name="email" placeholder="Email"/> </br>
       <input type="text" id="tel" class="span4" name="tel" placeholder="Telefono"/> </br>
+      
+      <?php echo $captcha['image']; ?><br/>
+      <input id="captcha" name="userCaptcha" placeholder="Captcha" type="text" />
+  
       <label class="checkbox"></label>
       <button type="submit" name="submit" class="btn btn-info btn-block">Registrarme</button>
       <br>
