39b3ce2b4459c685b2eb0bde2f10adc5d6014215
Jorge Mendiola <wrtfix@gmail.com>
2018-06-12 18:02:14 -0400

    Retraso tiempo de visualizacion de marquesina

--- application/views/layouts/portada_layout.php
+++ application/views/layouts/portada_layout.php
@@ -242,7 +242,7 @@
 		    //gap in pixels between the tickers
 		    gap: 250,
 		    //time in milliseconds before the marquee will start animating
-		    delayBeforeStart: 0,
+		    delayBeforeStart: 2000,
 		    //'left' or 'right'
 		    direction: 'left',
 		    //true or false - should the marquee be duplicated to show an effect of continues flow
