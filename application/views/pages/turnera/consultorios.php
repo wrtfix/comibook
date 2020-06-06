a52de546cde9f2c198041664e976fcdeab850af7
jmendiola <jmendiola@technisys.com>
2020-06-06 10:54:07 -0300

    Agrego validacion existencia de local

--- application/views/pages/turnera/consultorios.php
+++ application/views/pages/turnera/consultorios.php
@@ -19,8 +19,11 @@
 
 <?php $cont = 0; while($cont < count($agregados)) { ?>
 <input type="radio" name="idConsultorio" value="<?php print_r($agregados[$cont]->idConsultorio); ?>" > <?php print_r($agregados[$cont]->nombre); ?><br>
-<?php $cont++;}?>
+<?php $cont++;} if ($cont == 0) {?>
+<p>Debe crear un local si desea visualizar la agenda. Si desea dar de alta un local haga clik <a href="<?php echo site_url('/turnera/consultorio/index'); ?>">aqui</a> </p>
+<?php }else{ ?>
         
 <hr>    
 <button type="submit" id="guardar" class="btn btn-danger">Continuar</button>
+<?php } ?>
 </form>
