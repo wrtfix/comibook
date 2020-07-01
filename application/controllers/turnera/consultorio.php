5d8c42272f47be682d497a7e22a3c16e35e74565
jmendiola <jmendiola@technisys.com>
2020-07-01 00:06:58 -0300

    COMIBOOK-11: aplico cambio de nombre a titulo

--- application/controllers/turnera/consultorio.php
+++ application/controllers/turnera/consultorio.php
@@ -4,7 +4,8 @@ class Consultorio extends CI_Controller
 	public function __construct()
 	{
 		parent:: __construct();
-		$this->layout->placeholder("title", "Sistema de Gestion de Contenidos");
+                $this->load->model('configuraciones', '', TRUE);
+                $this->layout->placeholder("title", $this->configuraciones->getConfiguracion("SITE_NAME")[0]->valor);
 		$this->load->model('consultorios','',TRUE);
 		$this->load->spark('markdown-extra/0.0.0');
 	}
